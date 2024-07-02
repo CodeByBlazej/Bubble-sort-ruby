8926b1183b158344745d3211cb4dd81eb90fea59
CodeByBlazej <codebyblazej@gmail.com>
2024-07-02 19:24:35 +0100

    add more logic to array loop trying to solve exercise

--- app.rb
+++ app.rb
@@ -7,11 +7,25 @@ def bubble_sort (array)
   sorted_array = []
 
   array.each_with_index do |num, index|
-    if num[index] > num[index + 1]
+    if sorted_array.none?
       sorted_array.push(num)
-      # num[index] = num[index + 1]
+      # break if sorted_array.one?
+    else
+      sorted_array.each do |sorted_number|
+        if sorted_number > num
+          sorted_array.unshift(num)
+          break
+        end
+      end
     end
   end
+
+  #   if num > num[index]
+  #     sorted_array.push(num)
+  #     break if sorted_array.length == 2
+  #     # num[index] = num[index + 1]
+  #   end
+  # end
 
   # sorted = array.reduce(Array.new(0)) {|result, num| }
 
