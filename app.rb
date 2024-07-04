9096aa9c35c582404dec7a7d1551d51c7b607243
CodeByBlazej <codebyblazej@gmail.com>
2024-07-04 18:00:52 +0100

    add each_cons enumerable

--- app.rb
+++ app.rb
@@ -6,21 +6,21 @@ def bubble_sort (array)
 
   sorted_array = []
 
-  array.each_with_index do |num, index|
-    if sorted_array.none?
-      sorted_array.push(num)
-      # break if sorted_array.one?
-    else
-      sorted_array.each do |sorted_number|
-        if sorted_number > num
-          sorted_array.unshift(num)
-        elsif sorted_number < num
-          sorted_array.push(num)
-          break
-        end
-      end
-    end
-  end
+  # array.each_with_index do |num, index|
+  #   if sorted_array.none?
+  #     sorted_array.push(num)
+  #     # break if sorted_array.one?
+  #   else
+  #     sorted_array.each do |sorted_number|
+  #       if sorted_number > num
+  #         sorted_array.unshift(num)
+  #       elsif sorted_number < num
+  #         sorted_array.push(num)
+  #         break
+  #       end
+  #     end
+  #   end
+  # end
 
   #   if num > num[index]
   #     sorted_array.push(num)
@@ -30,6 +30,16 @@ def bubble_sort (array)
   # end
 
   # sorted = array.reduce(Array.new(0)) {|result, num| }
+
+  array.each_cons(2) do |first, second| 
+    if first > second
+      first = second
+      second = first
+    elsif first < second
+      sorted_array.push(first)
+      sorted_array.push(second)
+    end
+  end
 
   binding.pry
 
